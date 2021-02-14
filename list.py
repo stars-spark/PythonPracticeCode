0192d8fc0415b20111d597e0140fd24ad27b8520
lijiale-maxgit config --global user.email cleverlijiale@163.com <cleverlijiale@163.com>
2021-02-14 12:28:49 +0800

    .

--- list.py
+++ list.py
@@ -1,3 +1,9 @@
-word = ['a', 'b', 'c']
-word1 = word.append('d')
-print(word1)
+word = ['a']
+word.append('d')    #add end
+print(word)
+
+
+word.insert(0,'b')      #add
+
+del word[0]     #delrte
+print(word)
